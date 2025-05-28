d8ac8f6681cb7c48a35f1173a5022c6ae873ddda
yolkbot <octocat@nowhere.com>
2025-05-28 19:39:07 +0000

    update & sync data
    this is an automatic commit that updates shell shockers game data

--- src/constants/shellYoutube.js
+++ src/constants/shellYoutube.js
@@ -2,16 +2,6 @@
 export const ShellYoutube = [
     {
         "active": true,
-        "link": "https://www.youtube.com/watch?v=hp7WXq1PjeM",
-        "desc": "",
-        "imageExt": "",
-        "id": "hp7WXq1PjeM",
-        "author": "Godric Gryffindor",
-        "title": "Top 5 Most Popular Maps",
-        "externalImg": "https://i.ytimg.com/vi/hp7WXq1PjeM/hqdefault.jpg"
-    },
-    {
-        "active": true,
         "link": "https://www.youtube.com/watch?v=-Pj2j-PC020",
         "desc": "",
         "imageExt": "",
