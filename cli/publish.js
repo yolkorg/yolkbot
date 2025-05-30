110aab88451b9db4de46d2f9288fc36f7659cc95
thonk <hello@villainsrule.xyz>
2025-05-29 21:51:54 -0400

    tooling tweaks

--- cli/publish.js
+++ cli/publish.js
@@ -1,10 +1,10 @@
 /* eslint-disable camelcase */
 
-const webhook = process.env.NPM_DEV_WEBHOOK;
+const webhook = process.env.NPM_WEBHOOK;
 
 import fs from 'node:fs';
 
-const pkg = JSON.parse(fs.readFileSync(new URL('../../package.json', import.meta.url)));
+const pkg = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url)));
 
 const codeblock = '```';
 
@@ -14,16 +14,16 @@ await fetch(webhook, {
         embeds: [{
             author: {
                 name: `${pkg.name} ${pkg.version.replace(/-([a-zA-Z]+)\./, ' / $1 ')}`,
-                url: `https://www.npmjs.com/package/yolkbot/v/${pkg.version}`,
+                url: 'https://npmjs.com/yolkbot',
                 icon_url: 'https://cdn.discordapp.com/icons/1342571038063460443/620ed351a0e43104c4ec8cc4a8304870.png?size=128'
             },
             color: 39902,
-            description: `${codeblock}npm install ${pkg.name}@${pkg.version}${codeblock}${codeblock}bun add ${pkg.name}@${pkg.version}${codeblock}${codeblock}pnpm add ${pkg.name}@${pkg.version}${codeblock}\n**report cockroaches (& other bugs) in** <#1342571945123774527>`
+            description: `${codeblock}npm install ${pkg.name}@${pkg.version}${codeblock}${codeblock}bun add ${pkg.name}@${pkg.version}${codeblock}${codeblock}pnpm add ${pkg.name}@${pkg.version}${codeblock}\n**check <#1343337711473528862> for the ${pkg.version} changelog!**`
         }]
     }),
     headers: {
         'Content-Type': 'application/json'
     }
-})
+});
 
 console.log('\nsent to discord webhook!');
