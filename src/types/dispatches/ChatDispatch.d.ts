9d5056c3c50f4e441c7ae099d6efe494d48bb83f
thonk <hello@villainsrule.xyz>
2025-05-31 21:09:53 -0400

    1.0.16: properly track chat ratelimits

--- src/types/dispatches/ChatDispatch.d.ts
+++ src/types/dispatches/ChatDispatch.d.ts
@@ -2,9 +2,8 @@ import Bot from '../bot';
 
 export declare class ChatDispatch {
     msg: string;
-    noLimit: boolean;
 
-    constructor(msg: string, noLimit?: boolean);
+    constructor(msg: string);
 
     validate(bot: Bot): boolean;
     check(bot: Bot): boolean;
