f3c9f8afef38e3363d86bec12191b6f117037960
yolkbot <octocat@nowhere.com>
2025-06-09 16:22:30 +0000

    data modifications
    this is a fully automatic commit

--- src/constants/regions.js
+++ src/constants/regions.js
@@ -5,10 +5,6 @@ export const Regions = [
         "sub": "egs-static-live-useast-191w3fe4"
     },
     {
-        "id": "uswest",
-        "sub": "egs-static-live-uswest-192r5xds"
-    },
-    {
         "id": "singapore",
         "sub": "egs-static-live-singapore-5r545eei"
     },
@@ -27,5 +23,9 @@ export const Regions = [
     {
         "id": "sydney",
         "sub": "egs-static-live-sydney-302pa99o"
+    },
+    {
+        "id": "uswest",
+        "sub": "egs-static-live-uswest-192r5xds"
     }
 ];
