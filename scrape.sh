804855843a122c215b7a4ab3dbda630ac1fe1484
thonk <hello@villainsrule.xyz>
2025-06-09 10:10:49 -0400

    1.0.20: decrease workflow time 3x

--- scrape.sh
+++ scrape.sh
@@ -1,23 +1,19 @@
-printf "from JS...\n"
-
 bun cli/scrape/fromJS/challenges.js
-bun cli/scrape/fromJS/changelog.js
-bun cli/scrape/fromJS/items.js
-bun cli/scrape/fromJS/maps.js
-bun cli/scrape/fromJS/shopItems.js
+bun cli/scrape/fromJS/changelog.js & 
+bun cli/scrape/fromJS/items.js & 
+bun cli/scrape/fromJS/maps.js & 
+bun cli/scrape/fromJS/shopItems.js & 
 
-printf "\nfrom JSON...\n"
-
-bun cli/scrape/fromJSON/codes.js
-bun cli/scrape/fromJSON/housePromo.js
-bun cli/scrape/fromJSON/language.js
-bun cli/scrape/fromJSON/shellNews.js
-bun cli/scrape/fromJSON/shellYoutube.js
-bun cli/scrape/fromJSON/sounds.js
-
-printf "\nfrom WS...\n"
+bun cli/scrape/fromJSON/codes.js & 
+bun cli/scrape/fromJSON/housePromo.js & 
+bun cli/scrape/fromJSON/language.js & 
+bun cli/scrape/fromJSON/shellNews.js & 
+bun cli/scrape/fromJSON/shellYoutube.js & 
+bun cli/scrape/fromJSON/sounds.js & 
 
 bun cli/scrape/fromWS/matchmaker.js
 
+wait
+
 printf "\n"
-echo "scraping complete!"
+echo "all done!"
