bfa173e76f448e65490202ee0cd2e7adfab8b314
thonk <hello@villainsrule.xyz>
2025-06-09 21:25:16 -0400

    1.0.20: better VIP calculation, yolkws headers, type rawLoginData

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,12 @@
 import globals from './globals.js';
 import { IsBrowser, UserAgent } from './constants/index.js';
 
+const generateWebSocketKey = () => {
+    const array = new Uint8Array(16);
+    crypto.getRandomValues(array);
+    return btoa(String.fromCharCode(...array));
+}
+
 class yolkws extends globals.WebSocket {
     constructor(url, proxy) {
         if (IsBrowser) super(url);
@@ -8,8 +14,18 @@ class yolkws extends globals.WebSocket {
             super(url, {
                 agent: proxy ? new globals.SocksProxyAgent(proxy) : undefined,
                 headers: {
-                    'user-agent': UserAgent,
-                    'accept-language': 'en-US,en;q=0.9'
+                    'accept-encoding': 'gzip, deflate, br, zstd',
+                    'accept-language': 'en-US,en;q=0.9',
+                    'cache-control': 'no-cache',
+                    'connection': 'Upgrade',
+                    'host': url.split('/')[2],
+                    'origin': url.split('/')[0] + '//' + url.split('/')[2],
+                    'pragma': 'no-cache',
+                    'sec-websocket-extensions': 'permessage-deflate; client_max_window_bits',
+                    'sec-websocket-key': generateWebSocketKey(),
+                    'sec-websocket-version': '13',
+                    'upgrade': 'websocket',
+                    'user-agent': UserAgent
                 }
             })
         }
