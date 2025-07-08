32be159eecbddbb888325ecf48aa5f8b4049dcea
yolkbot <octocat@nowhere.com>
2025-07-08 22:17:22 +0000

    data modifications
    this is a fully automatic commit

--- src/constants/shellNews.js
+++ src/constants/shellNews.js
@@ -3,24 +3,24 @@ export const ShellNews = [
     {
         "content": "It's Summer Vacation! Relax and enjoy 🏖️",
         "imageExt": ".png",
-        "linksToVipStore": "1",
         "id": "news_mcmfgxwu3",
-        "elId": "BOYM9a",
-        "active": true
+        "elId": "jeaIAw",
+        "active": true,
+        "linksToChangeLog": "1"
     },
     {
         "content": "It's the Kart Bros Update! Try it now! 🏁 ",
         "imageExt": ".png",
         "active": true,
         "id": "news_mcmf9ivm1",
-        "elId": "RJFkJC",
+        "elId": "8vTT8x",
         "link": "https://kartbros.io/"
     },
     {
         "content": "🏈 Footballbros.io - Go Long!",
         "imageExt": ".png",
         "id": "news_mcmf9ivm2",
-        "elId": "4KRzms",
+        "elId": "rl8pRp",
         "link": "https://footballbros.io/?utm_source=ssnewsfeed&utm_medium=referral",
         "active": true
     },
@@ -28,7 +28,7 @@ export const ShellNews = [
         "content": "🤼 Get ready to rumble! Check out WrestleBros.io and beat up your bros! 🏆",
         "imageExt": ".png",
         "id": "news_m4ukw0jq5",
-        "elId": "5aa8yh",
+        "elId": "A7VRHi",
         "link": "https://wrestlebros.io/?utm_source=ssnewsfeed&utm_medium=referral",
         "active": true
     }
