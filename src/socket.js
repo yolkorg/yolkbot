9ca6baf55604d12e54c3fa458b0384a486ac459d
thonk <hello@villainsrule.xyz>
2025-07-13 15:03:39 -0400

    1.2.0: rewrite yolkws (closes #29)

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import globals from './globals.js';
-import { IsBrowser, UserAgent } from './constants/index.js';
+import { IsBrowser, ProxiesEnabled, UserAgent } from './constants/index.js';
 
 /*
 const generateWebSocketKey = () => {
@@ -9,19 +9,43 @@ const generateWebSocketKey = () => {
 }
 */
 
-class yolkws extends globals.WebSocket {
+class yolkws {
+    url = '';
+    proxy = '';
+
+    binaryType = '';
+
+    maxRetries = 5;
+
     constructor(url, proxy) {
-        if (IsBrowser) super(url);
-        else {
-            super(url, {
-                agent: proxy ? new globals.SocksProxyAgent(proxy) : undefined,
+        this.url = url;
+
+        if (!ProxiesEnabled && proxy) {
+            console.error('You cannot pass a proxy to a yolkws in this environment.');
+            process.exit(1);
+        }
+
+        this.proxy = proxy;
+    }
+
+    async tryConnect(tries = 1) {
+        const url = new URL(this.url);
+
+        const retryOrQuit = async () => {
+            if (tries === 5) return false;
+            return await this.tryConnect(tries + 1);
+        }
+
+        try {
+            this.socket = IsBrowser ? new globals.WebSocket(this.url) : new globals.WebSocket(this.url, {
+                agent: this.proxy ? new globals.SocksProxyAgent(this.proxy) : undefined,
                 headers: {
                     'accept-encoding': 'gzip, deflate, br, zstd',
                     'accept-language': 'en-US,en;q=0.9',
                     'cache-control': 'no-cache',
                     'connection': 'Upgrade',
-                    'host': url.split('/')[2],
-                    'origin': (url.split('/')[0] + '//' + url.split('/')[2]).replace('ws', 'http'),
+                    'host': url.host,
+                    'origin': url.origin.replace('ws', 'http'),
                     'pragma': 'no-cache',
                     'sec-websocket-extensions': 'permessage-deflate; client_max_window_bits',
                     // 'sec-websocket-key': generateWebSocketKey(),
@@ -29,8 +53,55 @@ class yolkws extends globals.WebSocket {
                     // 'upgrade': 'websocket',
                     'user-agent': UserAgent
                 }
-            })
+            });
+
+            if (this.binaryType) this.socket.binaryType = this.binaryType;
+        } catch (e) {
+            console.error(`Failed to connect on try ${tries}, trying again...`, e);
+            return await retryOrQuit();
         }
+
+        return new Promise((resolve) => {
+            const timeout = setTimeout(async () => {
+                if (IsBrowser) this.socket.close();
+                else this.socket.terminate();
+                resolve(await retryOrQuit());
+            }, 5000);
+
+            const errorListener = async (e) => {
+                clearTimeout(timeout);
+                console.error('WebSocket error', e);
+                resolve(await retryOrQuit());
+            };
+
+            this.socket.addEventListener('open', async () => {
+                clearTimeout(timeout);
+                this.socket.removeEventListener('error', errorListener);
+                resolve(true);
+            });
+
+            this.socket.addEventListener('error', errorListener);
+        })
+    }
+
+    get onmessage() {
+        return this.socket?.onmessage;
+    }
+
+    set onmessage(handler) {
+        if (this.socket) this.socket.onmessage = handler;
+    }
+
+    get onclose() {
+        return this.socket?.onclose;
+    }
+
+    set onclose(handler) {
+        if (this.socket) this.socket.onclose = handler;
+    }
+
+    get close() {
+        return this.socket?.close;
     }
 }
 
