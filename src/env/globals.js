80e6edafae07c09044c884fa4a2df1fd910e63bf
thonk <hello@villainsrule.xyz>
2025-08-16 21:32:52 -0400

    1.3.0: add a more version-compatible check for node

--- src/env/globals.js
+++ src/env/globals.js
@@ -5,7 +5,7 @@ const globals = {};
 const isBrowser = typeof window !== 'undefined' && typeof HTMLElement !== 'undefined';
 const isWorker = typeof WebSocketPair !== 'undefined' && typeof Cloudflare !== 'undefined';
 
-const isNode = typeof process !== 'undefined' && typeof navigator !== 'undefined' && navigator.userAgent?.startsWith('Node.js/');
+const isNode = typeof process !== 'undefined' && process.moduleLoadList.length > 0;
 const isDeno = typeof Deno !== 'undefined' && typeof navigator !== 'undefined' && navigator.userAgent?.startsWith('Deno/');
 const isBun = typeof Bun !== 'undefined' && typeof navigator !== 'undefined' && navigator.userAgent?.startsWith('Bun/');
 
