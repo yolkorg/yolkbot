a678940c9199c286aa35d5d7f3fd569554da7e84
thonk <hello@villainsrule.xyz>
2025-08-23 11:11:20 -0400

    1.3.0: use the `wwws` lib, fix iFetch

--- src/types/socket.d.ts
+++ src/types/socket.d.ts
@@ -1,7 +1,17 @@
-import NodeWebSocket from 'ws';
+type Data = string | Buffer | ArrayBuffer | Buffer[];
 
-declare class yolkws extends NodeWebSocket {
+declare class yolkws {
     constructor(url: string, proxy: string);
+
+    tryConnect($tries?: number): Promise<void>;
+
+    onmessage: (data: Data) => void;
+    onopen: () => void;
+    onclose: (code: number, reason: string) => void;
+    onerror: (err: Error) => void;
+
+    send(data: Data): void;
+    close(code?: number, reason?: string): void;
 }
 
 export default yolkws;
