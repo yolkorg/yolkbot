a1143c5a4d79830a0550dd3da944d67211326265
thonk <hello@villainsrule.xyz>
2025-08-23 20:39:35 -0400

    1.3.0: release

--- src/comm/CommIn.js
+++ src/comm/CommIn.js
@@ -11,69 +11,69 @@ export class CommIn {
         return Math.max(0, this.buffer.length - this.idx);
     }
 
-    // Unsigned 8-bit integer (0 to 255)
+    // unsigned 8-bit integer (0 to 255)
     static unPackInt8U() {
         const i2 = this.idx;
         this.idx++;
         return this.buffer[i2];
     }
 
-    // Signed 8-bit integer (-128 to 127)
+    // signed 8-bit integer (-128 to 127)
     static unPackInt8() {
         const v = this.unPackInt8U();
         return (v + 128) % 256 - 128;
     }
 
-    // Unsigned 16-bit integer (0 to 65535)
+    // unsigned 16-bit integer (0 to 65535)
     static unPackInt16U() {
         const i2 = this.idx;
         this.idx += 2;
         return this.buffer[i2] + this.buffer[i2 + 1] * 256;
     }
 
-    // Unsigned 24-bit integer (0 to 16777215)
+    // unsigned 24-bit integer (0 to 16777215)
     static unPackInt24U() {
         const i2 = this.idx;
         this.idx += 3;
         return this.buffer[i2] + this.buffer[i2 + 1] * 256 + this.buffer[i2 + 2] * 65536;
     }
 
-    // Unsigned 32-bit integer (0 to 2^32-1)
+    // unsigned 32-bit integer (0 to 2^32-1)
     static unPackInt32U() {
         const i2 = this.idx;
         this.idx += 4;
         return this.buffer[i2] + this.buffer[i2 + 1] * 256 + this.buffer[i2 + 2] * 65536 + this.buffer[i2 + 3] * 16777216;
     }
 
-    // Unsigned 8-bit integer (0 to 255)
+    // unsigned 8-bit integer (0 to 255)
     static unPackInt16() {
         const v = this.unPackInt16U();
         return (v + 32768) % 65536 - 32768;
     }
 
-    // Unsigned 32-bit integer (-2^31 to 2^31-1)
+    // unsigned 32-bit integer (-2^31 to 2^31-1)
     static unPackInt32() {
         const v = this.unPackInt32U();
         return (v + 2147483648) % 4294967296 - 2147483648;
     }
 
-    // Unsigned radians (0 to 6.2831)
+    // unsigned radians (0 to 6.2831)
     static unPackRadU() {
         return this.unPackInt24U() / 2097152;
     }
 
-    // Signed radians (-3.1416 to 3.1416)
+    // signed radians (-3.1416 to 3.1416)
     static unPackRad() {
         const v = this.unPackInt16U() / 8192;
         return v - Math.PI;
     }
 
-    // Float value packing (-327.68 to 327.67)
+    // float (-327.68 to 327.67)
     static unPackFloat() {
         return this.unPackInt16() / 256;
     }
 
-    // Double value packing (-32768 to 32767)
+    // double (-32768 to 32767)
     static unPackDouble() {
         return this.unPackInt32() / 1048576;
     }
