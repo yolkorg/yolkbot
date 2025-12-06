8f1180b9835d6df9a03f7588c4fbfcf893fd7f2a
thonk <hello@villainsrule.xyz>
2025-12-06 11:07:20 -0500

    1.5.0: merge wasm util.js w/ legacy.js

--- src/types/wasm/util.d.ts
+++
@@ -1,9 +0,0 @@
-export interface MovementXY {
-    movementX: number;
-    movementY: number;
-}
-
-export declare const sensitivity: number;
-
-export declare const normalizeYaw: (yaw: number) => number;
-export declare const calculateMovements: (currentYaw: number, currentPitch: number, targetYaw: number, targetPitch: number) => MovementXY;
